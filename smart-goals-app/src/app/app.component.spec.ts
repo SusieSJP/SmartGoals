279e7cdffa003a65d84f0d1b4d50ac87a2d9acf8
Susie Sun <sunjiaping1992@gmail.com>
2019-09-20 11:37:08 -0400

    change app2 to app

--- smart-goals-app/src/app/app.component.spec.ts
+++ smart-goals-app/src/app/app.component.spec.ts
@@ -1,17 +1,16 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+    TestBed
+        .configureTestingModule({
+          imports: [RouterTestingModule],
+          declarations: [AppComponent],
+        })
+        .compileComponents();
   }));
 
   it('should create the app', () => {
@@ -20,16 +19,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'smart-goals-app2'`, () => {
+  it(`should have as title 'smart-goals-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('smart-goals-app2');
+    expect(app.title).toEqual('smart-goals-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('smart-goals-app2 app is running!');
+    expect(compiled.querySelector('.content span').textContent)
+        .toContain('smart-goals-app app is running!');
   });
 });
